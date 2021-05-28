ca9ecbb08c681aa752df1f780777b6cb0ef3688a
Kirill <kirillmorgunov@mail.ru>
2021-05-28 18:49:53 +0300

    собирает главную страницу

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://fb.ru/misc/i/gallery/88575/2586156.jpg" width="30" height="30" alt="" />
+        </header>
+    )
+}
+
+export default Header;
