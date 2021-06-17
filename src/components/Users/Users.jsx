6319661c4a6d782200c638788a73d50a857b6525
Kirill remote <kirillmorgunov@mail.ru>
2021-06-17 15:50:46 +0300

    51 урок, добавление axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,27 +1,34 @@
+import * as axios from 'axios';
 import React from 'react';
 import classes from './Users.module.css';
 
 const Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                { id: 1, name: "Dmitry", status: "I'm a boss", location: {city: "Volgograd", country: "Russia"}, avatar: "", isFollowed: true},
-                { id: 2, name: "Alex", status: "I'm a boss too", location: {city: "Tampa", country: "USA"}, avatar: "", isFollowed: true},
-                { id: 3, name: "Marta", status: "Yo yo", location: {city: "Moscow", country: "Russia"}, avatar: "", isFollowed: false},      
-                { id: 4, name: "Antuane", status: "Hey", location: {city: "Paris", country: "France"}, avatar: "", isFollowed: false},    
-            ]
-        )
+        axios.get('https://react-social-6dbbe-default-rtdb.europe-west1.firebasedatabase.app/users1.json').then(response => {
+             props.setUsers(response.data['-McOwLH1FkyhOcQsdlrH'])
+        });
     }
+
+    // let createStore = async () => {        
+    //     axios.post('https://react-social-6dbbe-default-rtdb.europe-west1.firebasedatabase.app/users1.json',   [
+    //         { id: 1, name: "Dmitry", status: "I'm a boss", location: {city: "Volgograd", country: "Russia"}, avatar: "", isFollowed: true},
+    //         { id: 2, name: "Alex", status: "I'm a boss too", location: {city: "Tampa", country: "USA"}, avatar: "", isFollowed: true},
+    //         { id: 3, name: "Marta", status: "Yo yo", location: {city: "Moscow", country: "Russia"}, avatar: "", isFollowed: false},      
+    //         { id: 4, name: "Antuane", status: "Hey", location: {city: "Paris", country: "France"}, avatar: "", isFollowed: false},    
+    //     ])
+    // }
+
+    // createStore();
   
     return (
         <div>
             <h2 className={classes.title}>Users</h2>
             {
-                props.users.map(user => (
+                props.users.map(user => (                        
                         <div className={classes.user} key={user.id}>
-                            <div className={classes.left}>
-                                <img src="https://c0.klipartz.com/pngpicture/7/618/gratis-png-ilustracion-de-hombre-icono-de-avatar-avatar-de-moda-hombre.png" alt="avatar" width="100" height="100" />
+                            <div className={classes.left}>                        
+                                <img src={user.avatar ? user.avatar : "https://c0.klipartz.com/pngpicture/7/618/gratis-png-ilustracion-de-hombre-icono-de-avatar-avatar-de-moda-hombre.png" } alt="avatar" width="100" height="100" />
                                 <button onClick={() => {props.toggleFollow(user.id)}}>
                                     {user.isFollowed ? "Follow" : "UnFollow" }                                    
                                 </button>
